5c4414f9c9d8abed1d9b0a9fe29ad07d287b6e01
Antônio Marcos <77894621+anttonimarco@users.noreply.github.com>
2021-02-05 22:00:19 -0300

    Game_01

--- jogo_snake_01.py
+++ jogo_snake_01.py
@@ -1,4 +1,4 @@
-import pygame   # Importando pygame
+import pygame  # Importando pygame
 
 pygame.init()
 
@@ -6,34 +6,34 @@ horizontal = 400
 vertical = 300
 velocidade = 5
 
-janela = pygame.display.set_mode((800, 600))    # Definindo a variável que vai receber o display.
-pygame.display.set_caption('Game Python')   # Nome da janela.
+janela = pygame.display.set_mode((800, 600))  # Definindo a variável que vai receber o display.
+pygame.display.set_caption('Game Python')  # Nome da janela.
 
-janela_on = True    # variável janela_on recendo valor booleano (True) Verdadeiro.
+janela_on = True  # variável janela_on recendo valor booleano (True) Verdadeiro.
 
-while janela_on:    # Enquanto variável janela_on for verdadeiro(True) Faça isso.
-    pygame.time.delay(50) # Delay para poder visualizar a janela.
+while janela_on:  # Enquanto variável janela_on for verdadeiro(True) Faça isso.
+    pygame.time.delay(50)  # Delay para poder visualizar a janela.
 
-    for event in pygame.event.get():    # Para o evento teclar(get)press.
-        if event.type == pygame.QUIT:   # Se clicar em fechar a janela Faça:
-            janela_on = False   # Passe a variável janela_on para Falso(False)
+    for event in pygame.event.get():  # Para o evento teclar(get)press.
+        if event.type == pygame.QUIT:  # Se clicar em fechar a janela Faça:
+            janela_on = False  # Passe a variável janela_on para Falso(False)
             # Seguindo a lógica a variável se tornando falsa ele sai do for.
 
-    comandos = pygame.key.get_pressed() # variável comandos, recebendo um comando de uma tecla.
-    if comandos[pygame.K_UP]:   # Se a variável comandos receber a tecla para cima apertada faça.
-            vertical -= velocidade
+    comandos = pygame.key.get_pressed()  # variável comandos, recebendo um comando de uma tecla.
+    if comandos[pygame.K_UP]:  # Se a variável comandos receber a tecla para cima apertada faça.
+        vertical -= velocidade
 
     if comandos[pygame.K_DOWN]:
-            vertical += velocidade
+        vertical += velocidade
 
     if comandos[pygame.K_RIGHT]:
-            horizontal -= velocidade
+        horizontal += velocidade
 
     if comandos[pygame.K_LEFT]:
-            horizontal += velocidade
+        horizontal -= velocidade
 
     janela.fill((0, 0, 0))
-    pygame.draw.circle(janela, (0, 255, 0), (400, 300), 50) # Criação do objeto.
-    pygame.display.update() # Atualiza a tela para aparecer o objeto.
+    pygame.draw.circle(janela, (0, 255, 0), (horizontal, vertical), 50)  # Criação do objeto.
+    pygame.display.update()  # Atualiza a tela para aparecer o objeto.
 
 pygame.quit()
